de09c9606107a857362e05cf15fb5a8e89c70e88
Illés Alexander <illes.alexander88@gmail.com>
2019-04-03 13:45:41 +0200

    Update: componentdidmount and componentWillUnmount implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
 
 function EatOne(props) {
   return <button id="decrease" onClick={props.change}> Eat One </button>;
@@ -10,7 +11,6 @@ function BuyOne(props) {
 function AppWrapper(props) {
   return <div className="app-wrapper" onKeyDown={props.changeKey} tabIndex="1">{props.children}</div>;
 }
-
 function Display(props) {
   return <div className="display">{props.name}</div>;
 }
@@ -23,39 +23,45 @@ class GoldenAcornApp extends React.Component {
     this.state = {counter: 0};
   }
   render() {
+    console.log(this.state.counter);
     return (
-      <AppWrapper changeKey={this.changeValueWithKey}>
+      <AppWrapper >
         <BuyOne change={this.changeValue} />
         <Display name={this.state.counter} />
         <EatOne change={this.changeValue} />
       </AppWrapper>
     );
   }
+  componentDidMount(){
+    window.addEventListener('keydown', this.changeValueWithKey);
+  }
+  componentWillUnmount(){
+    window.removeEventListener("keydown", this.changeValueWithKey); 
+  }
   changeValue(e){  
-    console.log(e.keyCode);
-    if(this.state.counter!==0){
+    if(this.state.counter>0){
       e.target.id === "decrease"? 
-      this.setState({ counter: this.state.counter-=1 }):
-      this.setState({ counter: this.state.counter+=1 })
-      console.log(e.keyCode, e.target.id);
+      this.setState((prevState) => ({counter: prevState.counter-1})):
+      this.setState((prevState) => ({counter: prevState.counter+1}))
     }else{
       e.target.id === "increase"? 
-      this.setState({ counter: this.state.counter+=1 }):
+      this.setState((prevState) => ({counter: prevState.counter+1})):
       null
     }
   }
   changeValueWithKey(e){
-    console.log(e.keyCode);
-    if(this.state.counter!==0){
+    if(this.state.counter>0){
       e.keyCode===40? 
-      this.setState({ counter: this.state.counter-=1 }):
-      this.setState({ counter: this.state.counter+=1 })
+      this.setState((prevState) => ({counter: prevState.counter-1})):
+      e.keyCode===38? this.setState((prevState) => ({counter: prevState.counter+1})):
+      null
     }else{
       e.keyCode===38? 
-      this.setState({ counter: this.state.counter+=1 }):
+      this.setState((prevState) => ({counter: prevState.counter+1})):
       null
     }
   }
 }
 
 ReactDOM.render(<GoldenAcornApp />, document.getElementById("root"));
+
